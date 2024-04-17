9ac29146a84b3d161e231320a1d24415082b3459
Jackson-Vieira <jacksonvieiira12@gmail.com>
2024-04-17 06:27:04 -0300

    feat: word level transcription

--- whisper-streaming/server.py
+++ whisper-streaming/server.py
@@ -2,11 +2,9 @@ from aiohttp import web
 from typing import Final
 from faster_whisper import WhisperModel
 import aiohttp_cors
-import whisper
-import numpy as np
 import logging
-import tempfile
-import base64
+import numpy as np
+from numpy import ndarray
 
 logging.basicConfig()
 logging.getLogger("faster_whisper").setLevel(logging.DEBUG)
@@ -15,57 +13,41 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 SERVER_PORT: Final = 8000
-MODEL_SIZE: Final = "small"
+MODEL_SIZE: Final = "medium"
+VAD_FILTER: Final = False
 
 app = web.Application()
 model = WhisperModel(MODEL_SIZE, device="cuda", compute_type="float16")
 
 
-def process_wav_bytes(
-    webm_bytes: bytes, sample_rate: int = 16000, chunk_size: int = 48000
-):
-    with tempfile.NamedTemporaryFile(suffix=".wav", delete=True) as temp_file:
-        temp_file.write(webm_bytes)
-        temp_file.flush()
-        waveform = whisper.load_audio(temp_file.name, sr=sample_rate)
+def transcribe(array_data: ndarray) -> str:
+    segments, _ = model.transcribe(
+        array_data,
+        language="pt",
+        vad_filter=True,
+        condition_on_previous_text=True,
+    )
 
-        num_chunks = max(len(waveform) // chunk_size, 1)
-        chunked_waveform = np.array_split(waveform, num_chunks)
-
-        logger.info(f"Processing {len(chunked_waveform)} chunks")
-
-        return chunked_waveform
-
-
-def transcribe_chunk(chunk: bytes) -> str:
-    # chunk is a base64 encoded string
-    chunk_base64 = base64.b64decode(chunk)
-    chunks = process_wav_bytes(chunk_base64)
-
-    transcription = ""
-    for chunk in chunks:
-        # chunk = chunk.reshape(1, -1)
-        # processed_chunk = whisper.pad_or_trim(chunk)
-        segments, _ = model.transcribe(
-            chunk, language="pt", condition_on_previous_text=False, vad_filter=True
-        )
-        for segment in segments:
-            transcription += segment.text
+    transcription = [segment.text for segment in segments]
+    transcription = " ".join(transcription)
 
     return transcription
 
 
-# make a curl request to test this
-# curl -X POST http://localhost:8000/whisper -H "Content-Type: application/json" -d '{"chunk": "base64"}'
-async def hello(request):
-    response = await request.json()
-    chunk = response["chunk"]
-    logging.info(f"Transcribing chunk: {chunk}")
-    transcription = transcribe_chunk(chunk)
+# 1. Latencia
+# 2. Disponibilidade
+# 3. Escalabilidade
+
+
+async def handle(request):
+    buffer = await request.content.read()
+    buffer_array = np.frombuffer(buffer, dtype=np.float32)
+
+    transcription = transcribe(buffer_array)
     return web.json_response({"transcription": transcription})
 
 
-app.add_routes([web.post("/whisper", hello)])
+app.add_routes([web.post("/whisper", handle)])
 
 cors = aiohttp_cors.setup(
     app,
